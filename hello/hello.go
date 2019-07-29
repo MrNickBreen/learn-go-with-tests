117e4884357160d0e9df46687b58d00e4b7ec8d0
Nick <nickbreen@khanacademy.org>
2019-07-29 14:25:17 -0700

    More updates to the hello function

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,35 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+// Neat! We can declare the return variable as a named variable
+// and just call return
+// Also note this function is private because it's lowercase! Uppercase is public!!
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
